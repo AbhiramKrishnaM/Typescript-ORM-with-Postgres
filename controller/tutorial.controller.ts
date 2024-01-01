366d1e3b38e5732f356ea5001687d6c5b831d346
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2024-01-01 17:58:18 +0530

    added action methods

--- controller/tutorial.controller.ts
+++ controller/tutorial.controller.ts
@@ -12,4 +12,18 @@ export default class TutorialController {
    * deleteAll
    * findAllPublished
    */
+
+  async create(req: Request, res: Response) {}
+
+  async findAll(req: Request, res: Response) {}
+
+  async findOne(req: Request, res: Response) {}
+
+  async update(req: Request, res: Response) {}
+
+  async delete(req: Request, res: Response) {}
+
+  async deleteAll(req: Request, res: Response) {}
+
+  async findAllPublished(req: Request, res: Response) {}
 }
