3167346d091c766bcf60ff60e178fe54c4ecd48a
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2024-01-01 17:55:51 +0530

    added express and express types added tutorial controller

---
+++ controller/tutorial.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import Tutorial from "../models/tutorial.model";
+import tutorialRepository from "../repository/tutorial.repository";
+
+export default class TutorialController {
+  /**
+   * Create
+   * findAll
+   * findOne
+   * update
+   * delete
+   * deleteAll
+   * findAllPublished
+   */
+}
